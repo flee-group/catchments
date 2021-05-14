bcd7b22501e9936080354aa01d34d3640a113300
Matthew Talluto <matthew.talluto@uibk.ac.at>
2021-05-14 15:37:08 +0200

    Corrected some site locations and names

--- mara/r/mara_delineation.r
+++ mara/r/mara_delineation.r
@@ -12,10 +12,20 @@ library(ggplot2)
 #	password = pw, dbname = dbname)
 # mara = fleedb_raster(fdb, "mara_dem")
 mara = raster("~/work/students/Lukas/dem_aea.tif")
-sites = readRDS("data/sites.rds")
 
+## lukas had some catchment areas incorrect
+## swap catchment area for 42 and 43; done for the eduts
+## same for 31 and 32
+# sites = readRDS("data/sites.rds")
+sites = readRDS("data/sites_edits.shp")
+
+# mara2 = projectRaster(mara, crs="+init=epsg:32736")
 
 outlet = c(1040958, -154588)
+# outlet2 = SpatialPoints(matrix(c(1040958, -154588), nrow=1))
+# crs(outlet2) = crs(mara)
+# outlet2 = spTransform(outlet2, crs(mara2))
+# mara_r = delineate(mara2, threshold = 1e6, outlet = coordinates(outlet2)[1,])
 mara_r = delineate(mara, threshold = 1e6, outlet = outlet)
 
 Tp = pixel_topology(mara_r)
@@ -23,6 +33,9 @@ Tp = pixel_topology(mara_r)
 
 options(mc.cores = 5)
 mara_vect = vectorise_stream(mara_r$stream, Tp)
+
+st_write(mara_vect, "mara.shp")
+st_write(sites, "mara_sites.shp")
 
 ggplot(mara_vect) + geom_sf(col='blue') + 
 	geom_sf(data = sites, size=0.7, col='red') + 
